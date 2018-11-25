74104bd30d8e9886a209a00a8903dc740df2d2b9
nengcantique <43613075+nengcantique@users.noreply.github.com>
2018-11-25 22:21:49 +0700

    start n quit

--- Main.pl
+++ Main.pl
@@ -214,7 +214,27 @@ startPosition :-
 start :-
 /* Bimo */
     asserta(programStart),
-    print('Welcome!'), nl,
+    nl, print('              888888b.    .d88888b. 88888888888 888       888        d8888 888      .d8888b.  888    d8P  8888888 '),
+nl, print('              888  "88b  d88P" "Y88b    888     888   o   888       d88888 888     d88P  Y88b 888   d8P     888   '),
+nl, print('              888  .88P  888     888    888     888  d8b  888      d88P888 888     Y88b.      888  d8P      888   '),
+nl, print('              8888888K.  888     888    888     888 d888b 888     d88P 888 888      "Y888b.   888d88K       888   '),
+nl, print('              888  "Y88b 888     888    888     888d88888b888    d88P  888 888         "Y88b. 8888888b      888   '),
+nl, print('              888    888 888     888    888     88888P Y88888   d88P   888 888           "888 888  Y88b     888   '),
+nl, print('              888   d88P Y88b. .d88P    888     8888P   Y8888  d8888888888 888     Y88b  d88P 888   Y88b    888   '),
+nl, print('              8888888P"    Y88888P      888     888P     Y888 d88P     888 88888888 "Y8888P"  888    Y88b 8888888 '),
+nl,
+nl,
+nl,
+
+nl, print('              $$$$$$$$  $$    $$  $$$$$$$   $$$$$$$$  $$$$$$$   $$$$$$  $$$$$$$$  $$    $$   $$$$$$   $$$$$$$$'),
+nl, print('              $$        $$    $$  $$    $$  $$        $$    $$    $$    $$        $$$   $$  $$    $$  $$'),
+nl, print('              $$         $$  $$   $$    $$  $$        $$    $$    $$    $$        $$$$  $$  $$        $$'),   
+nl, print('              $$$$$       $$$$    $$$$$$$   $$$$$     $$$$$$$     $$    $$$$$     $$ $$ $$  $$        $$$$$'),   
+nl, print('              $$         $$  $$   $$        $$        $$    $$    $$    $$        $$  $$$$  $$        $$'),
+nl, print('              $$        $$    $$  $$        $$        $$    $$    $$    $$        $$   $$$  $$    $$  $$'),      
+nl, print('              $$$$$$$$  $$    $$  $$        $$$$$$$$  $$    $$  $$$$$$  $$$$$$$$  $$    $$   $$$$$$   $$$$$$$$'),
+nl,
+nl,
     randomize, wipeData, startPosition,
     help,
     nl.
@@ -227,8 +247,14 @@ start :-
 /* --------- quit ----------------------------------------------------- */
 quit :-
     print('Farewell, it was a good game!'),
+    print('Botwalski Experience © KataBimoBebas'),nl,
+    print('Created by : '), nl,
+    print('Bimo Aditya '), nl,
+    print('Irena Irmalasari '), nl,
+    print('Nada Afra S '), nl,
+    print('Fitria Budi A '), nl,
     halt.
-/* 
+
 /* --------- save & load ------------------------------------------------ */
 /*Fitria*/
 
